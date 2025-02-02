d25ce3c6063c142134d4b95733fa4d3eb0c499e3
carmenscar <carmenscartezini@gmail.com>
2025-02-01 21:48:00 -0300

    adjusting train_model.py

--- starter/data/data_clean.py
+++ starter/data/data_clean.py
@@ -5,23 +5,18 @@ logging.basicConfig(filename='logs.log',
                     level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-def import_data(dataframe):
+def import_data_and_clean(csv_path):
     try:
-        df = pd.read_csv(dataframe)
+        df = pd.read_csv(csv_path)
         logging.info("SUCCESS: dataframe is loaded")
-        return df
     except:
         logging.error("ERROR: dataframe not found")
         return None
-
-def clean_data(dataframe):
-    df = dataframe.copy()
     df.columns = df.columns.str.replace(' ', '')
     logging.info("SUCCESS: dataframe is cleaned")
     return df
 
 
 if __name__ == "__main__":
-    df_raw = import_data("/home/carmenscar/nd0821-c3-starter-code/starter/data/census.csv")
-    df_cleaned = clean_data(df_raw)
-    print(df_cleaned.head())
+    df_raw = import_data_and_clean("/home/carmenscar/nd0821-c3-starter-code/starter/data/census.csv")
+    print(df_raw.head())
