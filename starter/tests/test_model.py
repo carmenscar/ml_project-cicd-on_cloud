11316a3742bab7ae5c529f7eb54f16bd92cdb343
carmenscar <carmenscartezini@gmail.com>
2025-02-03 13:48:44 -0300

    adjusting fixture

--- starter/tests/test_model.py
+++ starter/tests/test_model.py
@@ -7,10 +7,6 @@ import sys
 import os
 import joblib
 
-base_path = os.path.dirname(__file__)
-model_path = os.path.join(base_path, '..', 'model')
-model_file_path = os.path.join(model_path, 'random_forest_model.pkl')
-
 from starter.ml.model import train_model, compute_model_metrics, inference
 
 @pytest.fixture
@@ -18,21 +14,27 @@ def data():
     """Fixture to create data"""
     X_train, y_train = make_classification(n_samples=100, n_features=10, n_classes=2, random_state=42)
     X_test, y_test = make_classification(n_samples=30, n_features=10, n_classes=2, random_state=42)
-    model = train_model(X_train, y_train)
+    rf_model = train_model(X_train, y_train)
     
-    return X_train, y_train, X_test, y_test, model
+    return X_train, y_train, X_test, y_test, rf_model
 
-def test_train_model(data):
+@pytest.fixture
+def model():
+    base_path = os.path.dirname(__file__)
+    model_path = os.path.join(base_path, '..', 'model')
+    model_file_path = os.path.join(model_path, 'random_forest_model.pkl')
+    model = joblib.load(model_file_path)
+    return model
+
+def test_train_model(data,model):
     """Testing if model was saved and correcly trained"""
     X_train, y_train, _, _, _ = data
-    model = joblib.load(model_file_path)
-    
     assert isinstance(model, RandomForestClassifier)
 
 def test_compute_model_metrics(data):
     """Testing if metrics are floats"""
-    _, _, X_test, y_test, model = data
-    preds = inference(model, X_test)
+    _, _, X_test, y_test, rf_model = data
+    preds = inference(rf_model, X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
     assert isinstance(precision, float)
     assert isinstance(recall, float)
